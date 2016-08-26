d610fe672f3c1b517f27e7f7c18692953aa61589
modderme123 <Milo@p-2.local>
2016-08-26 08:16:50 +0100

    Merge milo-branch into master
    Added basic canvas animation to Main.scala
    Added front and hidden login panel to index.html
    Fixed html bug for closing brackets

--- client/src/main/scala/Main.scala
+++ client/src/main/scala/Main.scala
@@ -12,42 +12,61 @@ import GameServerMessages._
 import GameClientMessages._
 
 object TryMe extends JSApp {
+  var time = 0
+  val socket = new WebSocket("ws://localhost:2345/game")
   val gameCanvas = document.getElementById("game-c").asInstanceOf[html.Canvas]
   val ctx = gameCanvas.getContext("2d").asInstanceOf[dom.CanvasRenderingContext2D]
+  object size {
+    val width = window.innerWidth
+    val height = window.innerHeight
+  }
 
-  def main(): Unit = {
+  def draw() = {
+    ctx.fillStyle = "white"
+    ctx.fillRect(0, 0, size.width, size.height)
+    ctx.fill()
 
-    gameCanvas.width = window.innerWidth
-    gameCanvas.height = window.innerHeight
-
-    ctx.moveTo(10,10)
-    ctx.lineTo(20,20)
-
+    ctx.beginPath()
+    ctx.moveTo(10, 10)
+    ctx.lineTo(
+      size.width - (time%size.width),
+      size.height - (time%size.height)
+    )
     ctx.stroke()
 
-    val socket = new WebSocket("ws://localhost:2345/game")
+    time += 1
+  }
+
+  def setup() = {
+    gameCanvas.width = size.width
+    gameCanvas.height = size.height
+
     val join = Join("Emmett")
     val msg = join.asJson.spaces2
-    console.log("main")
+
     socket.onopen = {event:Event =>
-      console.log("socket open")
       socket.send(msg)
     }
     socket.onerror = { event: ErrorEvent =>
       console.log(s"Failed: code: ${event}")
     }
-    socket.onmessage = { event: MessageEvent =>
-      val msg = event.data.toString
-      console.log(s"received message: $msg")
-      ClientProtocol.decodeMessage(msg) match {
-        case Some(state:State) => receivedState(state)
-        case None => console.log(s"unexpected message: $msg")
-      }
-    }
     socket.onclose = { event: Event =>
       console.log(s"socket closed ")
     }
+  }
 
+  def main(): Unit = {
+    setup()
+    window.setInterval(draw _, 10)
+  }
+
+  socket.onmessage = { event: MessageEvent =>
+    val msg = event.data.toString
+    console.log(s"received message: $msg")
+    ClientProtocol.decodeMessage(msg) match {
+      case Some(state:State) => receivedState(state)
+      case None => console.log(s"unexpected message: $msg")
+    }
   }
 
   def receivedState(state:State): Unit = {
