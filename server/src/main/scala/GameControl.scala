b8baa026c80df62a8e90154756989d3ccb6fe948
lee <lee@underneath.ca>
2016-08-31 23:29:09 +0100

    rework gravity to only pull down add some skidding to turn limit rotation to 90 degrees make playfield larger fix some intellij warnings rename to turret

--- server/src/main/scala/GameControl.scala
+++ server/src/main/scala/GameControl.scala
@@ -26,14 +26,14 @@ class GameControl(api: AppHostApi) extends AppController {
 
   case class SnowballState(pos: Vec2d, size: Double, speed: Vec2d) extends PlayfieldObject
 
-  val playField = PlayField(500, 500)
+  val playField = PlayField(1000, 800)
   var sleds = mutable.Map[ConnectionId, SledState]()
   val trees = randomTrees()
   val snowballs = mutable.ListBuffer[SnowballState]()
   val users = mutable.Map[ConnectionId, User]()
   var lastTime = System.currentTimeMillis()
   val turnDelta = Math.PI / 20
-  val maxSpeed = 200
+  val maxSpeed = 350
 
   api.tick(50 milliseconds) {
     gameTurn()
@@ -74,47 +74,85 @@ class GameControl(api: AppHostApi) extends AppController {
   /** received a client message */
   def message(id: ConnectionId, msg: String): Unit = {
     read[GameServerMessage](msg) match {
-      case TurnLeft => turn(id, -turnDelta)
-      case TurnRight => turn(id, turnDelta)
+      case TurnLeft   => turn(id, -turnDelta)
+      case TurnRight  => turn(id, turnDelta)
       case Join(name) => userJoin(id, name)
-      case Mouse(pos) => rotateCannon(id, pos)
-      case x => println(s"received unhandled client message: $x")
+      case Mouse(pos) => rotateTurret(id, pos)
+      case x          => println(s"received unhandled client message: $x")
     }
   }
 
-  private def rotateCannon(id: ConnectionId, pos: Position): Unit = {
-    sleds.get(id).map { sled =>
-      sleds(id) = sled.copy(turretRotation = -Math.atan2(pos.x - sled.pos.x, pos.y - sled.pos.y))
-    }.getOrElse {
-      println(s"where's the sled to rotate cannon for id: $id")
-    }
-  }
-
-  private def turn(id: ConnectionId, direction: Double): Unit = {
-    // TODO limit turn rate to 1 turn / 50msec
+  private def rotateTurret(id: ConnectionId, pos: Position): Unit = {
     sleds.get(id) match {
       case Some(sled) =>
-        sleds(id) = sled.copy(rotation = sled.rotation + direction)
-      case None =>
+        sleds(id) = sled.copy(turretRotation = -Math.atan2(pos.x - sled.pos.x, pos.y - sled.pos.y))
+      case None       =>
+        println(s"where's the sled to rotate turret for id: $id")
+    }
+  }
+
+  /** Apply a user initiated rotation to a single sled */
+  private def turn(id: ConnectionId, direction: Double): Unit = {
+    // TODO limit turn rate to e.g. 1 turn / 50msec
+    val maxRight = math.Pi / 2
+    val maxLeft = -math.Pi / 2
+    sleds.get(id) match {
+      case Some(sled) =>
+        val baseRotation = sled.rotation + direction
+        val rotation =
+          if (baseRotation > maxRight) maxRight
+          else if (baseRotation < maxLeft) maxLeft
+          else baseRotation
+
+        sleds(id) = sled.copy(rotation = rotation)
+      case None       =>
         println(s"where's the sled to turn for connection id: $id")
     }
   }
 
-  /** increase the speed of sleds due to gravity */
-  private def applyGravity(): Unit = {
-    val gravity = 20.0
-    sleds = sleds.map { case (id, sled) =>
+  /** Increase the speed of sleds due to gravity */
+  private def applyGravity(deltaSeconds: Double): Unit = {
+    val gravity = 100.0
+    val gravityFactor = gravity * deltaSeconds
+    mapSleds { sled =>
       val delta = Vec2d(
-        x = Math.sin(sled.rotation) * gravity,
-        y = Math.cos(sled.rotation) * gravity
+        x = 0,
+        y = math.cos(sled.rotation) * gravityFactor
       )
       val newSpeed = (sled.speed + delta).min(maxSpeed)
-      id -> sled.copy(speed = newSpeed)
+      sled.copy(speed = newSpeed)
     }
   }
 
-  /** constrain a value between 0 and a max value.
-    * values past one border are wrapped out the other side */
+  /** Adjust the speed based on the current rotation */
+  private def skidToRotate(deltaSeconds: Double): Unit = {
+    val skidSpeed = .75 // seconds to complete a skid
+    val skidFactor = math.min(1.0, deltaSeconds * skidSpeed)
+    mapSleds { sled =>
+      val speed = sled.speed.length
+      val current = sled.speed / speed
+      val target = Vec2d(
+        x = math.sin(sled.rotation),
+        y = math.cos(sled.rotation)
+      )
+      val skidVector = current + ((target - current) * skidFactor)
+      val newSpeed = skidVector * speed
+      sled.copy(speed = newSpeed)
+    }
+  }
+
+  /** Run a function that replaces each sled */
+  private def mapSleds(fn: SledState => SledState): Unit = {
+    sleds = sleds.map { case (id, sled) =>
+      id -> fn(sled)
+    }
+  }
+
+  private def slowFriction(sled: SledState): SledState = ???
+
+  /** Constrain a value between 0 and a max value.
+    * values past one border of the range are wrapped to the other side
+    * @return the wrapped value */
   private def wrapBorder(value: Double, max: Double): Double = {
     if (value > max * 2.0)
       max
@@ -137,14 +175,11 @@ class GameControl(api: AppHostApi) extends AppController {
   }
 
   /** move movable objects to their new location for this time period */
-  private def moveObjects(): Unit = {
-    val currentTime = System.currentTimeMillis()
-    val deltaSeconds = (currentTime - lastTime) / 1000.0
-    lastTime = currentTime
-    sleds = sleds.map { case (id, sled) =>
+  private def moveObjects(deltaSeconds: Double): Unit = {
+    mapSleds { sled =>
       val moved = sled.pos + (sled.speed * deltaSeconds)
       val wrapped = wrapInPlayfield(moved)
-      id -> sled.copy(pos = wrapped)
+      sled.copy(pos = wrapped)
     }
   }
 
@@ -152,15 +187,27 @@ class GameControl(api: AppHostApi) extends AppController {
     // TODO
   }
 
-  /** called to update game state on a regular timer */
+  /** Advance to the gmae clock to the next time slice
+    * @return the time in seconds since the last time slice
+    */
+  private def nextTimeSlice(): Double = {
+    val currentTime = System.currentTimeMillis()
+    val deltaSeconds = (currentTime - lastTime) / 1000.0
+    lastTime = currentTime
+    deltaSeconds
+  }
+
+  /** Called to update game state on a regular timer */
   private def gameTurn(): Unit = {
-    applyGravity()
-    moveObjects()
+    val deltaSeconds = nextTimeSlice()
+    applyGravity(deltaSeconds)
+    skidToRotate(deltaSeconds)
+    moveObjects(deltaSeconds)
     checkCollisions()
     api.sendAll(write(currentState()))
   }
 
-  /** initialize a set of playfield obstacles */
+  /** Initialize a set of playfield obstacles */
   private def randomTrees(): Set[TreeState] = {
     val sparsity = 100000
     val num = playField.width * playField.height / sparsity
@@ -169,7 +216,7 @@ class GameControl(api: AppHostApi) extends AppController {
     }.toSet
   }
 
-  /** package up the state to communicate to the client */
+  /** Package the relevant state to communicate to the client */
   private def currentState(): State = {
     val clientSleds = sleds.map { case (id, sledState) =>
       val user = users.getOrElse(id, User("???"))
