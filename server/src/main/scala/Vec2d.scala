a2b6a2b66591e5dd9e5993613fdddc5e51cc0f8c
lee <lee@underneath.ca>
2016-08-31 23:04:42 +0100

    add / and length on Vec2d.

--- server/src/main/scala/Vec2d.scala
+++ server/src/main/scala/Vec2d.scala
@@ -4,12 +4,16 @@ case class Vec2d(x: Double, y: Double) {
   def -(other: Vec2d): Vec2d = Vec2d(x - other.x, y - other.y)
 
   def *(d: Double): Vec2d = Vec2d(x * d, y * d)
+  
+  def /(d: Double): Vec2d = Vec2d(x / d, y / d)
+  
+  def length: Double = math.sqrt(x * x + y * y)
 
-  def max(other: Vec2d): Vec2d = Vec2d(Math.max(x, other.x), Math.max(y, other.y))
+  def max(other: Vec2d): Vec2d = Vec2d(math.max(x, other.x), math.max(y, other.y))
 
-  def max(m: Double): Vec2d = Vec2d(Math.max(x, m), Math.max(y, m))
+  def max(m: Double): Vec2d = Vec2d(math.max(x, m), math.max(y, m))
 
-  def min(other: Vec2d): Vec2d = Vec2d(Math.min(x, other.x), Math.min(y, other.y))
+  def min(other: Vec2d): Vec2d = Vec2d(math.min(x, other.x), math.min(y, other.y))
 
-  def min(m: Double): Vec2d = Vec2d(Math.min(x, m), Math.min(y, m))
+  def min(m: Double): Vec2d = Vec2d(math.min(x, m), math.min(y, m))
 }
