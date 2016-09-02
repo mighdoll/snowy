0ec7809a700e256f4370aa2621e240463eeb1301
modderme123 <modderme123@gmail.com>
2016-09-02 10:18:55 +0100

    Draw trees

--- client/src/main/scala/Main.scala
+++ client/src/main/scala/Main.scala
@@ -157,10 +157,37 @@ object ClientMain extends JSApp {
 
   //Draw a tree on the canvas
   def drawTree(pos: GameClientProtocol.Position): Unit = {
-    ctx.fillStyle = "rgb(30, 184, 22)"
+    val x = pos.x
+    val y = pos.y
+    val branchSize = 100
+
+    ctx.fillStyle = "rgb(94, 153, 105)"
     ctx.beginPath()
-    ctx.fillRect(pos.x, pos.y, 10, 10)
+    ctx.moveTo(x, y - branchSize * 2)
+    ctx.lineTo(x - branchSize * 1 / 2, y - branchSize * 1 / 2)
+    ctx.lineTo(x + branchSize * 1 / 2, y - branchSize * 1 / 2)
+    ctx.closePath()
     ctx.fill()
+
+    ctx.fillStyle = "rgb(56, 85, 58)"
+    ctx.beginPath()
+    ctx.fillRect(x - branchSize * 1 / 16, y - branchSize * 3 / 4, branchSize * 1 / 8, branchSize * 3 / 4)
+    ctx.fill()
+
+    ctx.translate(x, y - branchSize * 29 / 50)
+    ctx.rotate(Math.PI*13/18)
+
+    ctx.beginPath()
+    ctx.fillRect(0, 0, branchSize * 1 / 20, branchSize * 43 / 200)
+    ctx.fill()
+
+    ctx.rotate(Math.PI*5/9)
+    ctx.beginPath()
+    ctx.fillRect(0, branchSize * 7 / 200, branchSize * 1 / 20, branchSize * 1 / 4)
+    ctx.fillRect(branchSize * -1 / 10, branchSize * -13 / 200, branchSize * 1 / 20, branchSize * 1 / 4)
+    ctx.fill()
+
+    ctx.setTransform(1, 0, 0, 1, 0, 0)
   }
 
   def drawSnowball(pos: GameClientProtocol.Position, size: Double): Unit = {
