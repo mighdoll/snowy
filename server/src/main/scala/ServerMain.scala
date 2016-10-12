670380463452b3c4cadc4619ed2f6de5b24de8d0
lee <lee@underneath.ca>
2016-10-12 22:21:39 +0100

    rename to ServerMain

--- server/src/main/scala/ServerMain.scala
+++ server/src/main/scala/ServerMain.scala
@@ -1,7 +1,6 @@
-import akka.actor.ActorSystem
 import socketserve.WebServer.socketApplication
 
-object SnowyServer {
+object ServerMain {
   def main(args: Array[String]): Unit = {
     socketApplication(new GameControl(_))
   }
