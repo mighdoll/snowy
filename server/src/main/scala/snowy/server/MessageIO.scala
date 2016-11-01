f0a2883855726a33a5bc8d1207f99374c1954598
lee <lee@underneath.ca>
2016-11-01 23:32:20 +0000

    cosmetic

--- server/src/main/scala/snowy/server/MessageIO.scala
+++ server/src/main/scala/snowy/server/MessageIO.scala
@@ -10,7 +10,7 @@ import snowy.util.Perf.time
 
 /** A GameClientMessage wrapper over the send/receive api */
 class MessageIO(api: AppHostApi) {
-  // optimization for boopickle: don't track references, since don't send any graphs
+  // optimization for boopickle: don't track references, since we don't send any graphs
   implicit def pickleState = new PickleState(new EncoderSize, false, false)
 
   /** Send a message to the client */
