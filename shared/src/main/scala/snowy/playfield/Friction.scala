f3fc8caff2be2107d8e3626ee3c7a81b71f3587d
lee <lee@underneath.ca>
2016-11-04 15:16:17 +0000

    formatting

--- shared/src/main/scala/snowy/playfield/Friction.scala
+++ shared/src/main/scala/snowy/playfield/Friction.scala
@@ -12,31 +12,32 @@ object Friction {
 /** a slowdown applied against the direction of travel */
 class Friction(frictionMin: Double, frictionMax: Double) {
   def apply(current: Vec2d, rotation: Double): Vec2d = {
-    current.transform { case _ if !current.zero =>
-      // friction is min when skis are aligned with direction, max when skis are at 90 degrees
-      val speed = current.length
-      val direction = current.unit
-      val rotationVector = Vec2d.fromRotation(rotation)
-      val angleSkiToTravel = direction.angle(rotationVector)
-      val rotationFactor = pow(abs(sin(angleSkiToTravel)), brakeSteepness)
+    current.transform {
+      case _ if !current.zero =>
+        // friction is min when skis are aligned with direction, max when skis are at 90 degrees
+        val speed            = current.length
+        val direction        = current.unit
+        val rotationVector   = Vec2d.fromRotation(rotation)
+        val angleSkiToTravel = direction.angle(rotationVector)
+        val rotationFactor   = pow(abs(sin(angleSkiToTravel)), brakeSteepness)
 
-      // -speed in direction of travel
-      val sledFriction = frictionMax * rotationFactor
+        // -speed in direction of travel
+        val sledFriction = frictionMax * rotationFactor
 
-      val adjustedFriction = min(speed, max(frictionMin, sledFriction))
-      val newSpeed = direction * (speed - adjustedFriction)
-      newSpeed
+        val adjustedFriction = min(speed, max(frictionMin, sledFriction))
+        val newSpeed         = direction * (speed - adjustedFriction)
+        newSpeed
     }
   }
 }
 
 /** a force applied directly in the current direction of travel */
-class InlineForce(force: Double, maxSpeed:Double) {
+class InlineForce(force: Double, maxSpeed: Double) {
   def apply(current: Vec2d): Vec2d = {
-    current.transform { case _ if !current.zero =>
-      val speed = min(max(current.length + force, 0), maxSpeed)
-      current.unit * speed
+    current.transform {
+      case _ if !current.zero =>
+        val speed = min(max(current.length + force, 0), maxSpeed)
+        current.unit * speed
     }
   }
 }
-
