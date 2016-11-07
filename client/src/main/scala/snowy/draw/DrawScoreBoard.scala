3d8ac65979c0ef03868b38d40bdf0db0004f923d
modderme123 <modderme123@gmail.com>
2016-11-07 08:58:21 +0000

    Improve scoreboard

--- client/src/main/scala/snowy/draw/DrawScoreBoard.scala
+++ client/src/main/scala/snowy/draw/DrawScoreBoard.scala
@@ -5,18 +5,20 @@ import snowy.client.ClientDraw.{ctx, size}
 import vector.Vec2d
 
 class DrawScoreBoard(scale: Double, myName: String) {
-  def draw(scoreboard: Scoreboard) {
-    val scorescale  = Math.max(210 / size.x, 255 / size.y) * 2.5
-    val scorescaled = Vec2d(210, 225) / scorescale
-    val scorepos    = Vec2d(size.x * .99 - scorescaled.x, size.y * .01)
-    ctx.fillStyle = "rgba(255, 255, 255, 0.5)"
-    ctx.fillRect(scorepos.x, scorepos.y, scorescaled.x, scorescaled.y)
+  val scoresize   = Vec2d(210, 255)
+  val scorescale  = Math.max(scoresize.x / size.x, scoresize.y / size.y) * 2.5
+  val scorescaled = scoresize / scorescale
+  val scorepos    = Vec2d(size.x * .99 - scorescaled.x, size.y * .01)
+  ctx.fillStyle = "rgba(255, 255, 255, 0.5)"
+  ctx.fillRect(scorepos.x, scorepos.y, scorescaled.x, scorescaled.y)
 
-    ctx.fillStyle = "rgb(0, 0, 0)"
-    val fromLeft  = 10 * scale
-    val fromRight = 10 * scale
-    val fromTop   = 60 * scale
-    val inbetween = 20 * scale
+  ctx.fillStyle = "rgb(0, 0, 0)"
+  val fromLeft  = 10 * scale
+  val fromRight = 10 * scale
+  val fromTop   = 60 * scale
+  val inbetween = 20 * scale
+
+  def draw(scoreboard: Scoreboard) {
     ctx.font = (25 * scale) + "px Arial"
     ctx.textAlign = "center"
     ctx
