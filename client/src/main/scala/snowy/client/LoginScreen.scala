796a9e3ad0a66e35a2b8bd41207dfe7b878147c1
modderme123 <modderme123@gmail.com>
2016-11-11 15:32:38 +0000

    Ski colors now customizable from loginscreen

--- client/src/main/scala/snowy/client/LoginScreen.scala
+++ client/src/main/scala/snowy/client/LoginScreen.scala
@@ -30,12 +30,17 @@ object LoginScreen {
       Math.PI * 3 / 2,
       Math.PI / 2,
       sledKind,
-      lineColors.toString,
+      skiColor.color.toString,
       "rgb(120, 201, 44)")
   }
 
   private var connected: Option[Connection] = None
   private var drawLoop: Option[Int]         = None
+
+  private var skiColor: SkiColor            = BasicSkis
+  private val skiColors: Seq[SkiColor] =
+    Seq(BasicSkis, GreenSkis, RedSkis, YellowSkis)
+
   private var sledKind: SledKind            = BasicSled
   private val sledKinds: Seq[SledKind] =
     Seq(BasicSled, GunnerSled, TankSled, SpeedySled, SpikySled)
@@ -71,19 +76,35 @@ object LoginScreen {
           .remove("hide")
     }
   }
-  val text = document.querySelector("#class > .caption").asInstanceOf[html.Div]
-  document.querySelector("#class > .container > :last-child").asInstanceOf[html.Span].onclick = { event: Event =>
+
+  val classText = document.querySelector("#class > .caption").asInstanceOf[html.Div]
+  document.querySelector("#class > .container > :first-child").asInstanceOf[html.Span].onclick = { event: Event =>
     val currentIndex = sledKinds.indexOf(sledKind)
     sledKind =
       if (currentIndex < sledKinds.length - 1) sledKinds(currentIndex + 1)
       else sledKinds.head
-    text.innerHTML = "Class: " + sledKind.toString.replace("Sled", "")
+    classText.innerHTML = "Class: " + sledKind.toString.replace("Sled", "")
   }
   document.querySelector("#class > .container :last-child").asInstanceOf[html.Span].onclick = { event: Event =>
     val currentIndex = sledKinds.indexOf(sledKind)
     sledKind = if (currentIndex > 0) sledKinds(currentIndex - 1) else sledKinds.last
-    text.innerHTML = "Class: " + sledKind.toString.replace("Sled", "")
+    classText.innerHTML = "Class: " + sledKind.toString.replace("Sled", "")
   }
+
+  val colorText = document.querySelector("#skis > .caption").asInstanceOf[html.Div]
+  document.querySelector("#skis > .container > :first-child").asInstanceOf[html.Span].onclick = { event: Event =>
+    val currentIndex = skiColors.indexOf(skiColor)
+    skiColor =
+      if (currentIndex < skiColors.length - 1) skiColors(currentIndex + 1)
+      else skiColors.head
+    colorText.innerHTML = "Color: " + sledKind.toString.replace("Sled", "")
+  }
+  document.querySelector("#skis > .container :last-child").asInstanceOf[html.Span].onclick = { event: Event =>
+    val currentIndex = skiColors.indexOf(skiColor)
+    skiColor = if (currentIndex > 0) skiColors(currentIndex - 1) else skiColors.last
+    colorText.innerHTML = "Color: " + skiColor.toString.replace("Skis", "")
+  }
+
   //When the users sends the login form, send it as a username to the server
   document.getElementById("login-form").asInstanceOf[html.Form].onsubmit = {
     event: Event =>
@@ -94,7 +115,7 @@ object LoginScreen {
             new Connection(
               document.getElementById("username").asInstanceOf[html.Input].value,
               sledKind,
-              BasicSkis
+              skiColor
             )
           )
         case x if x.isDefined => connected.get.reSpawn()
