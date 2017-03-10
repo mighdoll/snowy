c1d45f7318df67c46050f1d90a38caac87a6ff7b
modderme123 <modderme123@gmail.com>
2017-03-10 12:08:46 +0900

    Health v1

--- client/src/main/scala/snowy/client/DrawState2.scala
+++ client/src/main/scala/snowy/client/DrawState2.scala
@@ -23,6 +23,12 @@ object DrawState2 {
     val leave2Geo = new ConeGeometry(24, 16, 4, 1, false, 0.8, Math.PI * 2)
 
     val snowballGeo = new THREE.BoxGeometry(2, 2, 2)
+
+    val healthLeft   = new THREE.CylinderGeometry(3.2, 3.2, 2, 6)
+    val healthMiddle = new THREE.BoxGeometry(64, 2, 6.4)
+
+    val healthLeft2   = new THREE.CylinderGeometry(1.6, 1.6, 4, 6)
+    val healthMiddle2 = new THREE.BoxGeometry(64, 4, 3.2)
   }
   object Mats {
     val sledMat = new THREE.MeshPhongMaterial(
@@ -78,12 +84,27 @@ object DrawState2 {
         )
         .asInstanceOf[MeshPhongMaterialParameters]
     )
+
+    val healthBorderMat = new THREE.MeshPhongMaterial(
+      Dynamic
+        .literal(
+          color = 0x444444
+        )
+        .asInstanceOf[MeshPhongMaterialParameters]
+    )
+    val healthColorMat = new THREE.MeshPhongMaterial(
+      Dynamic
+        .literal(
+          color = 0x21ce3a
+        )
+        .asInstanceOf[MeshPhongMaterialParameters]
+    )
   }
   object Meshes {
     val mainBody = new THREE.Mesh(Geos.sledGeo, Mats.sledMat)
     val ski1     = new THREE.Mesh(Geos.skiGeo, Mats.skiMat)
     val skiTip1  = new THREE.Mesh(Geos.skiTipGeo, Mats.skiTipMat)
-    val ski2 = Meshes.ski1.clone()
+    val ski2     = Meshes.ski1.clone()
     val skiTip2  = Meshes.skiTip1.clone()
 
     val turret = new THREE.Mesh(Geos.turretGeo, Mats.turretMat)
@@ -93,11 +114,19 @@ object DrawState2 {
     val leave2 = new THREE.Mesh(Geos.leave2Geo, Mats.leave2Mat)
 
     val snowball = new THREE.Mesh(Geos.snowballGeo, Mats.snowballMat)
+
+    val healthLeft   = new THREE.Mesh(Geos.healthLeft, Mats.healthBorderMat)
+    val healthRight  = healthLeft.clone()
+    val healthMiddle = new THREE.Mesh(Geos.healthMiddle, Mats.healthBorderMat)
+
+    val healthLeftIn   = new THREE.Mesh(Geos.healthLeft2, Mats.healthColorMat)
+    val healthRightIn  = healthLeftIn.clone()
+    val healthMiddleIn = new THREE.Mesh(Geos.healthMiddle2, Mats.healthColorMat)
   }
   object Bodies {
-    val sled = new THREE.Object3D()
-    val tree = new THREE.Object3D()
-
+    val sled   = new THREE.Object3D()
+    val tree   = new THREE.Object3D()
+    val health = new THREE.Object3D()
   }
   object Groups {
     val ctrees     = new THREE.Object3D()
@@ -109,7 +138,7 @@ object DrawState2 {
   val light = new THREE.DirectionalLight(0xffffff)
   val grid  = new THREE.GridHelper(4000, 50)
 
-  Meshes.turret.position.set(0,0,-5)
+  Meshes.turret.position.set(0, 0, -5)
   Bodies.sled.add(Meshes.mainBody)
   Bodies.sled.add(Meshes.turret)
 
@@ -118,7 +147,6 @@ object DrawState2 {
 
   Meshes.skiTip2.position.set(0, 0, 1.5 - 0.25 / 2)
   Meshes.ski2.add(Meshes.skiTip2)
-
 
   Meshes.ski1.position.set(-1.25, -2.5, 0)
   Meshes.ski2.position.set(1.25, -2.5, 0)
@@ -133,6 +161,29 @@ object DrawState2 {
   Bodies.tree.add(Meshes.trunk)
   Bodies.tree.add(Meshes.leave1)
   Bodies.tree.add(Meshes.leave2)
+
+  Meshes.healthLeft.position.x = -32 + 2
+  Meshes.healthRight.position.x = 32 - 2
+
+  Meshes.healthLeftIn.position.x = (-32 + 2) * 0.9
+  Meshes.healthRightIn.position.x = (32 - 2) * 0.9
+
+  Meshes.healthLeftIn.scale.set(0.9, 1, 0.9)
+  Meshes.healthMiddleIn.scale.set(0.9, 1, 0.9)
+  Meshes.healthRightIn.scale.set(0.9, 1, 0.9)
+
+  Bodies.health.add(Meshes.healthLeft)
+  Bodies.health.add(Meshes.healthMiddle)
+  Bodies.health.add(Meshes.healthRight)
+
+  Bodies.health.add(Meshes.healthLeftIn)
+  Bodies.health.add(Meshes.healthMiddleIn)
+  Bodies.health.add(Meshes.healthRightIn)
+
+  //Bodies.health.scale.set(0.4, 1, 0.4)
+
+  scene.add(Bodies.health)
+
   def setup(): Unit = {
     scene.add(amb)
 
@@ -154,6 +205,7 @@ object DrawState2 {
     scene.add(Groups.csnowballs)
 
     scene.add(Groups.csleds)
+
   }
   //TODO: Remove trees after dead
   def drawState(snowballs: Store[Snowball],
@@ -260,6 +312,9 @@ object DrawState2 {
       }
     }
 
+    Bodies.health.position.set(mySled._position.x, 0, mySled._position.y + 60)
+    Bodies.health.setRotationFromAxisAngle(new THREE.Vector3(1, 0, 0), 0.1 * math.Pi)
+
     Meshes.mainBody.scale.x = mySled.radius
     Meshes.mainBody.scale.y = mySled.radius
     Meshes.mainBody.scale.z = mySled.radius
@@ -286,7 +341,7 @@ object DrawState2 {
       !ids.contains(sled.name)
     }
   }
-  def removeAll(): Unit ={
+  def removeAll(): Unit = {
     scene.remove(amb)
     scene.remove(light)
     scene.remove(Bodies.sled)
