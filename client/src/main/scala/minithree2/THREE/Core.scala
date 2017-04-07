726909ed63ed00e569ae277e1bec8a4278d9dd45
modderme123 <modderme123@gmail.com>
2017-04-06 20:45:07 -0700

    Complete buffer geometry

--- client/src/main/scala/minithree2/THREE/Core.scala
+++ client/src/main/scala/minithree2/THREE/Core.scala
@@ -50,3 +50,118 @@ class BufferAttribute(var array: ArrayBuffer,
   def setXYZ(index: Int, x: Double, y: Double, z: Double): Unit             = js.native
   def setXYZW(index: Int, x: Double, y: Double, z: Double, w: Double): Unit = js.native
 }
+
+@ScalaJSDefined
+trait DrawRangeParameters extends js.Object {
+  val start: js.UndefOr[Int] = js.undefined
+  val count: js.UndefOr[Int] = js.undefined
+}
+
+@ScalaJSDefined
+trait GroupsParameters extends js.Object {
+  val start: js.UndefOr[Int]         = js.undefined
+  val count: js.UndefOr[Int]         = js.undefined
+  val materialIndex: js.UndefOr[Int] = js.undefined
+}
+
+@js.native
+@JSGlobal("THREE.BufferGeometry")
+class BufferGeometry extends js.Object with EventDispatcher {
+  var attributes: js.Object                                        = js.native
+  var boundingBox: Box3                                            = js.native
+  var boundingSphere: Sphere                                       = js.native
+  var drawRange: DrawRangeParameters                               = js.native
+  var groups: GroupsParameters                                     = js.native
+  var id: Int                                                      = js.native
+  var index: BufferAttribute                                       = js.native
+  var isBufferGeometry: Boolean                                    = js.native
+  var MaxIndex: Int                                                = js.native
+  var morphAttributes: js.Object                                   = js.native
+  var name: String                                                 = js.native
+  var uuid: String                                                 = js.native
+  def addAttribute(name: String, attribute: BufferAttribute): Unit = js.native
+  def addGroup(start: Int, count: Int, materialIndex: Int): Unit   = js.native
+  def applyMatrix(matrix: Matrix4): Unit                           = js.native
+  def center(): Unit                                               = js.native
+  override def clone(): BufferGeometry                             = js.native
+  def copy(bufferGeometry: BufferGeometry): BufferGeometry         = js.native
+  def clearGroups(): Unit                                          = js.native
+  def computeBoundingBox(): Unit                                   = js.native
+  def computeBoundingSphere(): Unit                                = js.native
+  def computeVertexNormals(): Unit                                 = js.native
+  def dispose(): Unit                                              = js.native
+  def fromDirectGeometry(Geometry: Geometry): BufferGeometry       = js.native
+  def fromGeometry(Geometry: Geometry): BufferGeometry             = js.native
+  def getAttribute(name: String): BufferAttribute                  = js.native
+  def getIndex(): BufferAttribute                                  = js.native
+  def lookAt(vector: Vector3): BufferGeometry                      = js.native
+  def merge(bufferGeometry: BufferGeometry, offset: Int): Unit     = js.native
+  def normalizeNormals(): Unit                                     = js.native
+  def removeAttribute(name: String): BufferAttribute               = js.native
+  def rotateX(radians: Float): BufferGeometry                      = js.native
+  def rotateY(radians: Float): BufferGeometry                      = js.native
+  def rotateZ(radians: Float): BufferGeometry                      = js.native
+  def scale(x: Float, y: Float, z: Float): BufferGeometry          = js.native
+  def setIndex(index: BufferAttribute): Unit                       = js.native
+  def setDrawRange(start: Int, count: Int): Unit                   = js.native
+  def setFromObject(`object`: Object3D): BufferGeometry            = js.native
+  def toJSON(): js.Object                                          = js.native
+  def toNonIndexed(): BufferGeometry                               = js.native
+  def translate(x: Float, y: Float, z: Float): BufferGeometry      = js.native
+  def updateFromObject(`object`: Object3D): BufferGeometry         = js.native
+}
+
+@js.native
+@JSGlobal("THREE.Clock")
+class Clock extends js.Object {}
+
+@js.native
+@JSGlobal("THREE.DirectGeometry")
+class DirectGeometry extends js.Object {}
+
+@js.native
+trait EventDispatcher extends js.Object {}
+
+@js.native
+@JSGlobal("THREE.Face3")
+class Face3 extends js.Object {}
+
+@js.native
+@JSGlobal("THREE.Geometry")
+class Geometry extends js.Object {}
+
+@js.native
+@JSGlobal("THREE.InstancedBufferAttribute")
+class InstancedBufferAttribute extends js.Object {}
+
+@js.native
+@JSGlobal("THREE.InstancedBufferGeometry")
+class InstancedBufferGeometry extends js.Object {}
+
+@js.native
+@JSGlobal("THREE.InstancedInterleavedBuffer")
+class InstancedInterleavedBuffer extends js.Object {}
+
+@js.native
+@JSGlobal("THREE.InterleavedBuffer")
+class InterleavedBuffer extends js.Object {}
+
+@js.native
+@JSGlobal("THREE.InterleavedBufferAttribute")
+class InterleavedBufferAttribute extends js.Object {}
+
+@js.native
+@JSGlobal("THREE.Layers")
+class Layers extends js.Object {}
+
+@js.native
+@JSGlobal("THREE.Object3D")
+class Object3D extends js.Object {}
+
+@js.native
+@JSGlobal("THREE.Raycaster")
+class Raycaster extends js.Object {}
+
+@js.native
+@JSGlobal("THREE.Uniform")
+class Uniform extends js.Object {}
