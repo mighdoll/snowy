eb759cd8b6d4f002dbe6c9641c71c503f453db8c
Lee Mighdoll <lee@underneath.ca>
2017-04-11 07:08:41 +0900

    add simple rate limiting utility function

---
+++ server/src/main/scala/snowy/util/RateLimit.scala
@@ -0,0 +1,26 @@
+package snowy.util
+
+import scala.concurrent.duration.FiniteDuration
+
+object RateLimit {
+
+  /** @return a function that relays calls to a provided function no more than
+    * once per period
+    * @param finiteDuration mimimum time between calls
+    * @param fn function to call
+    */
+  def rateLimit(finiteDuration: FiniteDuration)(fn: => Unit): () => Unit = {
+    var started  = false
+    var lastTime = 0L
+
+    () =>
+      {
+        val current = System.nanoTime()
+        if (!started || current - lastTime > finiteDuration.toNanos) {
+          started = true
+          lastTime = current
+          fn
+        }
+      }
+  }
+}
---
+++ server/src/test/scala/snowy/util/TestRateLimit.scala
@@ -0,0 +1,19 @@
+package snowy.util
+
+import org.scalatest.PropSpec
+import org.scalatest.prop._
+import RateLimit.rateLimit
+import scala.concurrent.duration._
+
+class TestRateLimit extends PropSpec with PropertyChecks {
+
+  property("rate limit limits calls") {
+    var count = 0
+    val limited = rateLimit(10.milliseconds){
+      count = count + 1
+    }
+    (0 to 100).foreach(_ => limited())
+    assert(count == 1)
+  }
+
+}
