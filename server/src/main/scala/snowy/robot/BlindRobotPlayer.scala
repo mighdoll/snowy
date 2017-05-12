3d85aedb3cbc9a2f565ca0cc424b39c5616f798a
Lee Mighdoll <lee@underneath.ca>
2017-05-11 14:07:45 -1000

    add random turns to blind robot player for more realistic load tests

--- server/src/main/scala/snowy/robot/BlindRobotPlayer.scala
+++ server/src/main/scala/snowy/robot/BlindRobotPlayer.scala
@@ -3,21 +3,57 @@ package snowy.robot
 import java.util.concurrent.ThreadLocalRandom
 import com.typesafe.scalalogging.StrictLogging
 import snowy.GameServerProtocol._
+import socketserve.ActorTypes.Actors
+import scala.concurrent.duration._
+import akka.actor.ActorSystem
 
-class BlindRobotPlayer(api: RobotApi, userName: String)
+class BlindRobotPlayer[_: Actors](api: RobotApi, userName: String)
     extends Robot with StrictLogging {
 
-  val robotGameTime = System.currentTimeMillis()
+  private val twoPi         = math.Pi * 2
+  private var currentAngle  = 0d
+  private val robotGameTime = System.currentTimeMillis()
 
   api.sendToServer(Join(userName))
   api.sendToServer(Start(Shooting, robotGameTime))
-  api.sendToServer(TargetAngle(randomDouble() * math.Pi * 2))
+  updateAngle(randomDouble() * twoPi)
+
+  val actorSystem = implicitly[ActorSystem]
+  import actorSystem.dispatcher
+
+  actorSystem.scheduler.schedule(100 milliseconds, 20 milliseconds) {
+    randomTurn()
+  }
 
   override def refresh(state: RobotGameState): Unit = {}
 
   override def killed(): Unit = {
     logger.info(s"$userName killed")
     api.sendToServer(ReJoin)
+  }
+
+  private def randomTurn(): Unit = {
+    randomDouble() match {
+      case d if d < .1 =>
+        val delta = randomDouble() * .1
+        updateAngle(currentAngle - delta)
+      case d if d < .1 =>
+        val delta = randomDouble() * .1
+        updateAngle(currentAngle + delta)
+      case _ =>
+    }
+  }
+
+  private def roundAngle(angle: Double): Double = {
+    val modAngle = angle % twoPi
+    if (modAngle < 0)
+      twoPi + modAngle
+    else modAngle
+  }
+
+  private def updateAngle(angle:Double): Unit = {
+    currentAngle = roundAngle(angle)
+    api.sendToServer(TargetAngle(currentAngle))
   }
 
   private def randomDouble() = ThreadLocalRandom.current.nextDouble()
