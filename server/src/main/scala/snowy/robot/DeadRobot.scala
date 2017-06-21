6ca9a383e79c882312d5ad9c52ae9ff74d3ab26d
modderme123 <modderme123@gmail.com>
2017-06-21 08:10:03 +0200

    Add a robot that doesn't move for debugging

---
+++ server/src/main/scala/snowy/robot/DeadRobot.scala
@@ -0,0 +1,33 @@
+package snowy.robot
+
+import java.util.concurrent.ThreadLocalRandom
+
+import snowy.AllLists
+import snowy.GameServerProtocol._
+import snowy.playfield._
+import vector.Vec2d
+
+import scala.util.Random
+
+object DeadRobot {
+  private var id = 0
+
+  def apply(api: RobotApi): DeadRobot = {
+    id = id + 1
+    new DeadRobot(api, AllLists.allNames(Random.nextInt(AllLists.allNames.size)))
+  }
+}
+
+class DeadRobot(api: RobotApi, name: String) extends Robot {
+  val mySkis =
+    AllLists.allSkis(ThreadLocalRandom.current.nextInt(AllLists.allSkis.length))
+  val myType =
+    AllLists.allSleds(ThreadLocalRandom.current.nextInt(AllLists.allSleds.length))
+
+  api.sendToServer(Join(name, myType, mySkis))
+
+  def refresh(state: RobotGameState): Unit =
+    api.sendToServer(Start(Slowing, System.currentTimeMillis()))
+
+  def killed(): Unit = api.sendToServer(ReJoin)
+}
